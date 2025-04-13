2bdeadff045f4dac0dd4ec7eea0ca6bfc63ae8f8
Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
2025-04-13 15:08:11 +0100

    Add hiera-eyaml again

--- bin/install.sh
+++ bin/install.sh
@@ -8,7 +8,7 @@ BASE_DIR=$(dirname $BIN_DIR)
 
 # Configuration.
 
-SHELL_PACKAGES="bash-completion curl git-flow gpg vim wget xclip zip"
+SHELL_PACKAGES="bash-completion curl git-flow gpg hiera-eyaml vim wget xclip zip"
 NETWORK_PACKAGES="bind9-dnsutils inetutils-traceroute lsof ncat nmap socat whois"
 CHOCO="/c/ProgramData/chocolatey/bin/choco"
 CHOCO_PACKAGES="7zip firacode nmap openjdk wincrypt-sshagent winscp"
