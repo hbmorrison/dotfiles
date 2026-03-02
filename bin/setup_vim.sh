384b38af4260627a044a6378220247589233eee3
Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
2026-03-02 08:29:51 +0000

    Fix vim setup script

--- bin/setup_vim.sh
+++ bin/setup_vim.sh
@@ -3,76 +3,62 @@
 DOTFILES_ORIGIN_URL=$(git -C $BASE_DIR remote -v 2>/dev/null | awk '/(push)/ {print $2}')
 DOTFILES_ORIGIN_DIR=$(dirname $DOTFILES_ORIGIN_URL)
 DOTVIM_ORIGIN_URL="${DOTFILES_ORIGIN_DIR}/dotvim.git"
-DOTVIM_BRANCH=$(git -C $BASE_DIR branch --show-current)
+DOTFILES_BRANCH=$(git -C $BASE_DIR branch --show-current)
 VIM_DIR="${HOME}/.vim"
+
+# Do not change the branch if the dotfiles branch is a git flow branch.
+
+case $DOTFILES_BRANCH in
+  feature-*) ;&
+  bugfix-*)  ;&
+  release-*) ;&
+  hotfix-*)  ;&
+  support-*) DOTFILES_BRANCH="main" ;;
+esac
 
 # Check whether the vim directory already exists.
 
 if [ -d $VIM_DIR ]
 then
-
-  # Check whether the remote URL of the vim directory is correct.
-
   VIM_DIR_ORIGIN_URL=$(git -C $VIM_DIR remote -v 2>/dev/null | awk '/(push)/ {print $2}')
-  if [ "${VIM_DIR_ORIGIN_URL}" = $DOTVIM_ORIGIN_URL ]
+
+  # If it does, make sure that the git origin of the vim directory is correct.
+
+  notice "checking that $VIM_DIR git origin is correct"
+  [ "${VIM_DIR_ORIGIN_URL}" = $DOTVIM_ORIGIN_URL ] \
+   && pass || fail "git origin in ${VIM_DIR} does not match ${DOTVIM_ORIGIN_URL}"
+
+  # Check that the current branch is correct.
+
+  VIM_DIR_BRANCH=$(git -C $VIM_DIR branch --show-current 2>/dev/null)
+  if [ $VIM_DIR_BRANCH != $DOTFILES_BRANCH ]
   then
-
-    # Check that the current branch is correct.
-
-    VIM_DIR_BRANCH=$(git -C $VIM_DIR branch --show-current 2>/dev/null)
-    if [ $VIM_DIR_BRANCH != $DOTVIM_BRANCH ]
+    notice "updating vim directory branch to ${DOTFILES_BRANCH}"
+    if ! git -C $VIM_DIR checkout $DOTFILES_BRANCH &>/dev/null && pass || fail
     then
 
-      # If the branch is not correct, checkout the correct branch.
+      # Checkout the main branch as a fallback.
 
-      notice "Updating vim directory branch to ${DOTVIM_BRANCH}"
-      if ! git -C $VIM_DIR checkout $DOTVIM_BRANCH &>/dev/null
-      then
-        echo "Failed"
-
-        # If the checkout fails, checkout the main branch as a fallback.
-
-        notice "Updating vim directory branch to main"
-        if git -C $VIM_DIR checkout main &>/dev/null
-        then
-          echo "Done"
-        else
-          echo
-          echo "Error: run git -C $VIM_DIR checkout main"
-        fi
-      else
-        echo "Done"
-      fi
+      [ $DOTFILES_BRANCH != "main" ] \
+       && notice "updating vim directory branch to main" \
+       && git -C $VIM_DIR checkout main \
+        &>/dev/null && pass || fail "run git -C $VIM_DIR checkout main manually"
     fi
-  else
-    echo "Error: git origin in ${VIM_DIR} does not match ${DOTVIM_ORIGIN_URL}"
-    exit 1
   fi
-
 else
 
-  # If the vim directory does not exist, clone it.
+  # Clone the vim directory.
 
-  notice "Cloning the $DOTVIM_BRANCH branch of the vim directory"
-  if git clone -b $DOTVIM_BRANCH $DOTVIM_ORIGIN_URL $VIM_DIR &>/dev/null
+  notice "cloning the $DOTFILES_BRANCH branch of the vim directory"
+  if ! git clone -b $DOTFILES_BRANCH $DOTVIM_ORIGIN_URL $VIM_DIR &>/dev/null && pass || fail
   then
-    echo "Done"
-  else
-    echo "Failed"
 
     # If the clone fails, clone from the main branch as a fallback.
 
     notice "Cloning the default branch of the vim directory"
-    if git clone $DOTVIM_ORIGIN_URL $VIM_DIR &>/dev/null
-    then
-      echo "Done"
-    else
-      echo
-      echo "Error: run git clone $DOTVIM_ORIGIN_URL $VIM_DIR"
-      exit 1
-    fi
+    git clone $DOTVIM_ORIGIN_URL $VIM_DIR \
+     &>/dev/null && pass || fail "run git clone $DOTVIM_ORIGIN_URL $VIM_DIR"
   fi
-
 fi
 
 # Pull and update the submodules.
